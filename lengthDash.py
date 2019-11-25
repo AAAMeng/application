66f61c4853b915edc58024e1a9c2089d976d9bf7
aaaMeng <zhumeng@wrdtech.com>
2019-11-25 09:46:40 +0800

    [feature]

--- lengthDash.py
+++ lengthDash.py
@@ -21,18 +21,18 @@ app = dash.Dash(__name__)
 
 mydata = read_from_txt(fillna=False)
 x_data = list(mydata.keys())
-y_data = list(mydata.get(app).count(axis=1) for app in x_data)
+y_data = list(mydata.get(app).count(axis=1).values for app in x_data)
 app.layout = html.Div([
     html.Div([html.H1("Packet Length for Different Application")], style={"textAlign": "center"}),
     # dcc.Graph(id="my-graph",figure = fig),
     dcc.Graph(
         id='my-graph',
-        figure=ff.create_distplot(y_data, x_data, bin_size=10)
+        figure=ff.create_distplot(y_data, x_data, bin_size=20, histnorm='probability')
     ),
-    dcc.Graph(
-        id='my-graph-2',
-        figure=ff.create_distplot(y_data, x_data, bin_size=5)
-    )
+    # dcc.Graph(
+    #     id='my-graph-2',
+    #     figure=ff.create_distplot(y_data, x_data, bin_size=20, histnorm='probability')
+    # )
 ])
 
 
