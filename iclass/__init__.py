a5e39e5673f2ff759357207dace0d23bed1b7a2a
aaaMeng <zhumeng@wrdtech.com>
2019-11-27 13:26:28 +0800

    [fix] transform dict ro package

---
+++ iclass/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py.py    
+@Contact :   zhumeng@bupt.edu.cn
+@License :   (C)Copyright 2019 zhumeng
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2019/11/27 13:25      xm         1.0          None
+"""
+
+# import time
+# import numpy as np
+# import pandas as pd
