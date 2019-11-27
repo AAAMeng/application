c031d653a7a1732336f8376e53527d04470076fd
aaaMeng <zhumeng@wrdtech.com>
2019-11-27 16:46:29 +0800

    [test]

--- imodel/find_error.py
+++ imodel/find_error.py
@@ -225,5 +225,5 @@ for i in range(test_iter):
     alist = np.ones(4)*label_test[i]
     preLabel = sess.run(predictions["classes"], feed_dict={_X: np.reshape(data_test[i], (-1, 1600)),
                                                            y: np.reshape(alist, (-1, 4)), keep_prob: 1.0})
-    if preLabel != label_test[i]:
-        print(" error " + str(i) + ": ", label_test[i], "->", preLabel, "[ ", data_test[i], " ]")
+    if preLabel[0] != label_test[i]:
+        print(" Error " + str(i) + ": ", label_test[i], "->", preLabel[0], data_test[i])
