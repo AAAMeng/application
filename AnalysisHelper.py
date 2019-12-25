76da945b419b38d24bc38d21d986755f04df073e
aaaMeng <zhumeng@bupt.edu.cn>
2019-12-25 22:07:03 +0800

    [backup] 2019/12/25

--- AnalysisHelper.py
+++ AnalysisHelper.py
@@ -1,26 +1,19 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- encoding: utf-8 -*-
 """
-@File    :   result_show.py    
+@File    :   AnalysisHelper.py    
 @Contact :   zhumeng@bupt.edu.cn
 @License :   (C)Copyright 2019 zhumeng
 
 @Modify Time      @Author    @Version    @Desciption
 ------------      -------    --------    -----------
-2019/12/4 13:49      xm         1.0          None
+2019/12/25 21:09      xm         1.0          None
 """
-
-# 绘制列之间的相似度热度图
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from datetime import datetime
 import tensorflow as tf
-import tensorflow_datasets as tfds
-import time
-from iclass.dataSet import DataSet
-from sklearn.model_selection import train_test_split
 import os
 import pylab
 import io
@@ -42,9 +35,9 @@ app_label = {
     # 'app7': "b",
     # 'app8': "c"
 }
+# print(device_lib.list_local_devices())
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
-
-# function:随机选取每个文件4个样本绘制灰度图
 def data2feature(f_name):
     feature = f_name.to_numpy()
     feature = feature[:, 1:]  # first row is index_num
@@ -56,8 +49,41 @@ def data2feature(f_name):
 def read_data_mean():
     feature = []
     for fname in app_label.keys():
-        feature.append(data2feature(pd.read_csv("../../dataset/labeled_data/" + fname + ".csv")))
+        feature.append(data2feature(pd.read_csv(str(rootPath) + "dataset/labeled_data/" + fname + ".csv")))
     return feature
+
+
+def cal_similarity_between_label_and_predict(name, groups, features):
+    np.set_printoptions(linewidth=400)  # 设置输出不换行
+    error_feature = groups.to_numpy()
+    # batch of mis-prediction image
+    img0 = np.reshape(error_feature[:, 1:-2], (-1, 40, 40, 1))
+    img0 = tf.convert_to_tensor(img0)
+
+    # real label
+    img1 = np.array(features[name[0]][0:-1])
+    img1 = np.reshape(img1, (-1, 40, 40, 1))
+    img1 = tf.convert_to_tensor(img1)
+
+    # predict label
+    img2 = np.array(features[name[1]][0:-1])
+    img2 = np.reshape(img2, (-1, 40, 40, 1))
+    img2 = tf.convert_to_tensor(img2)
+
+    ssim1 = tf.image.ssim(img1, img0, max_val=255.0, filter_size=11,
+                          filter_sigma=1.5, k1=0.01, k2=0.03)
+    ssim2 = tf.image.ssim(img2, img0, max_val=255.0, filter_size=11,
+                          filter_sigma=1.5, k1=0.01, k2=0.03)
+    print("\nFor group", name)
+    print("real label SSIM: ", ssim1.numpy())
+    print("pred label SSIM: ", ssim2.numpy())
+
+    # Compute PSNR (Peak Signal-to-Noise Ratio) between a and b
+    psnr1 = tf.image.psnr(img1, img0, max_val=255)
+    psnr2 = tf.image.psnr(img2, img0, max_val=255)
+
+    print("real label PSNR: ", psnr1.numpy())
+    print("pred label PSNR: ", psnr2.numpy())
 
 
 def image_grid():
@@ -65,14 +91,15 @@ def image_grid():
     fig = plt.figure(dpi=600)
     # read the labeled_data
     features = read_data_mean()
-    error_df = pd.read_csv("../../dataset/false_clf/1128.csv")  # .to_numpy()[:, 1:]
+    error_df = pd.read_csv(str(rootPath) + "dataset/false_clf/1209.csv")  # .to_numpy()[:, 1:]
     grouped_by_label = error_df.groupby(['1600', '1601'])  # sort by (truelabel, prelabel)
     row = 0
     for name, groups in grouped_by_label:
+        cal_similarity_between_label_and_predict(name, groups, features)
         error_feature = groups.to_numpy()
         for i in range(len(error_feature)):
             plt.subplot(grouped_by_label.__len__(), grouped_by_label.size().max() + 2,
-                            row * (grouped_by_label.size().max() + 2) + (i + 3))  # (row, col, sub_pos)
+                        row * (grouped_by_label.size().max() + 2) + (i + 3))  # (row, col, sub_pos)
             data = error_feature[i, 1:-2]
             img = data.reshape([40, 40])
             plt.title(int(error_feature[i, -2]), fontsize=5, pad=5)  # true label
@@ -81,7 +108,7 @@ def image_grid():
 
         # Label image
         plt.subplot(grouped_by_label.__len__(), grouped_by_label.size().max() + 2,
-                        row * (grouped_by_label.size().max() + 2) + 1)
+                    row * (grouped_by_label.size().max() + 2) + 1)
         data = features[name[0]][0:-1]
         img = data.reshape([40, 40])
         plt.title(int(name[0]), fontsize=5, pad=5)  # name[0] is the true label
@@ -119,7 +146,12 @@ def plot_to_image(figure):
 
 
 if __name__ == "__main__":
-    # 绘制误分类样本的灰度图并与相关分类对比
+    # 绘制误分类样本的灰度图与正确分类和误分类类别对比
+    # 计算SSIM(结构相似性)和PSPR(峰值信噪比)评估图片相似度
+    # result is showing TensorBoard
+    # after run, cmd >>>tensorboard --logdir logs --host=10.3.220.200
+    # open web "http://10.3.220.200:6006/"
+
     logdir = str(rootPath) + "application/logs/train_data/" + datetime.now().strftime("%Y%m%d-%H%M%S")
     # Creates a file writer for the log directory.
     file_writer = tf.summary.create_file_writer(logdir)
